27623fbdfe39227dfaa44d72d323feee3a8fadce
Achille Dupont <achille.dupont@etudiant.univ-lr.fr>
2023-11-10 09:48:13 +0100

    refactor(home tech and user) : merge both home of user and technician

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/HomeController.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/HomeController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserHomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/home-user', name: 'app_user_home')]
-    public function index(): Response
+    public function indexUser(): Response
     {
         return $this->render('user_home/index.html.twig', [
             'controller_name' => 'UserHomeController',
+        ]);
+    }
+
+    #[Route('/home-tech', name: 'app_tech_home')]
+    public function indexTech(): Response
+    {
+        return $this->render('tech_home/index.html.twig', [
+            'controller_name' => 'TechHomeController',
         ]);
     }
 }

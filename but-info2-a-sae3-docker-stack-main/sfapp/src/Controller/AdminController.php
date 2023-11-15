0ecd3d5f038a8c4d6b246b01bfc239bbc23cc32d
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2023-11-16 00:16:13 +0100

    feat(room-pages): creating room list and room details pages with their routes on admincontroller

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/AdminController.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/AdminController.php
@@ -2,17 +2,66 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RoomRepository;
+use App\Repository\SensorRepository;
+use App\Repository\AcquisitionSystemRepository;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin-dashboard/room', name: 'app_admin_room')]
-    public function roomIndex(): Response
+    #[Route('/admin-dashboard/room-list', name: 'app_admin_room_list')]
+    //public function roomIndex(RoomRepository $roomRepository, SensorRepository $sensorRepository, AcquisitionSystemRepository $acquisitionSystemRepository): Response
+    public function roomIndex(ManagerRegistry $doctrine): Response
     {
+        // Plus optimisé mais pas vu en cours 
+        //
+        // $rooms = $roomRepository->findAll();
+        // $sensors = $sensorRepository->findAll();
+        // $acquisitionSystems = $acquisitionSystemRepository->findAll();
+
+        // return $this->render('admin/room-list.html.twig', [
+        //     'rooms' => $rooms,
+        //     'sensors' => $sensors,
+        //     'acquisitionSystems' => $acquisitionSystems,
+        // ]);
+
+        $entityManager = $doctrine->getManager();
+
+        $roomRepository = $entityManager->getRepository('App\Entity\Room');
+        $sensorRepository = $entityManager->getRepository('App\Entity\Sensor');
+        $acquisitionSystemRepository = $entityManager->getRepository('App\Entity\AcquisitionSystem');
+
+        $rooms = $roomRepository->findAll();
+        $sensors = $sensorRepository->findAll();
+        $acquisitionSystems = $acquisitionSystemRepository->findAll();
+
+        return $this->render('admin/room-list.html.twig', [
+            'rooms' => $rooms,
+            'sensors' => $sensors,
+            'acquisitionSystems' => $acquisitionSystems,
+        ]);
+    }
+
+    #[Route('/admin-dashboard/room/{name?}', name: 'app_admin_room')]
+    public function roomDetails(?string $name, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $roomRepository = $entityManager->getRepository('App\Entity\Room');
+        $sensorRepository = $entityManager->getRepository('App\Entity\Sensor');
+        $acquisitionSystemRepository = $entityManager->getRepository('App\Entity\AcquisitionSystem');
+
+        $room = $roomRepository->findOneBy(['name' => $name]);
+        $acquisitionSystem = $acquisitionSystemRepository->findOneBy(['room' => $room]);
+        $sensors = $sensorRepository->findBy(['acquisitionSystem' => $acquisitionSystem]);
+
         return $this->render('admin/room.html.twig', [
-            'controller_name' => 'AdminController',
+            'room' => $room,
+            'sensors' => $sensors,
+            'acquisitionSystem' => $acquisitionSystem,
         ]);
     }
 

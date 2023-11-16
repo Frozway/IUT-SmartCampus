41fd3c8ed7fa85de0d6dda92eeb1004f7983f92e
Ivan Bortnik <ivan.bortnik@etudiant.univ-lr.fr>
2023-11-16 09:09:03 +0100

    feat (admin dashboard): Add a custom alert style

---
+++ but-info2-a-sae3-docker-stack-main/sfapp/migrations/Version20231116073257.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231116073257 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE acquisition_system (id INT AUTO_INCREMENT NOT NULL, room_id INT DEFAULT NULL, name VARCHAR(10) NOT NULL, UNIQUE INDEX UNIQ_13C6162254177093 (room_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(10) NOT NULL, floor INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sensor (id INT AUTO_INCREMENT NOT NULL, acquisition_system_id INT DEFAULT NULL, name VARCHAR(10) NOT NULL, INDEX IDX_BC8617B0331785FF (acquisition_system_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, rank VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE acquisition_system ADD CONSTRAINT FK_13C6162254177093 FOREIGN KEY (room_id) REFERENCES room (id)');
+        $this->addSql('ALTER TABLE sensor ADD CONSTRAINT FK_BC8617B0331785FF FOREIGN KEY (acquisition_system_id) REFERENCES acquisition_system (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE acquisition_system DROP FOREIGN KEY FK_13C6162254177093');
+        $this->addSql('ALTER TABLE sensor DROP FOREIGN KEY FK_BC8617B0331785FF');
+        $this->addSql('DROP TABLE acquisition_system');
+        $this->addSql('DROP TABLE room');
+        $this->addSql('DROP TABLE sensor');
+        $this->addSql('DROP TABLE user');
+    }
+}

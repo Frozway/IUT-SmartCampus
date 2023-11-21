a7f5afafe28325f76853452bd3d9e74a2fd3e336
Mathys Besnard <mathys.besnard@etudiant.univ-lr.fr>
2023-11-21 16:36:18 +0100

    feat<login>: add login form

--- but-info2-a-sae3-docker-stack-main/sfapp/migrations/Version20231121132724.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/migrations/Version20231121132724.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231117083511 extends AbstractMigration
+final class Version20231121132724 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20231117083511 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD password VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL COMMENT \'(DC2Type:json)\', DROP rank');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP password');
+        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677 ON user');
+        $this->addSql('ALTER TABLE user ADD rank VARCHAR(50) NOT NULL, DROP roles');
     }
 }
--- but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/IndexController.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/IndexController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class IndexController extends AbstractController
 {
+//    #[Route('/', name: 'app_index')]
+//    public function index(): Response
+//    {
+//        return $this->render('index/index.html.twig', [
+//            'controller_name' => 'IndexController',
+//        ]);
+//    }
+
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_admin_dashboard')->send();
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }

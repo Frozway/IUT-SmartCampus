72c0931aaa9e612b4e5af87bcbd87a402d75acf4
Achille Dupont <achille.dupont@etudiant.univ-lr.fr>
2023-12-13 01:05:29 +0100

    feat (tech-dashboard) : list non installed AS and add button to install them in the app

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/TechController.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/TechController.php
@@ -5,14 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class TechController extends AbstractController
 {
-    #[Route('/tech', name: 'app_tech')]
-    public function index(): Response
+    /**
+     * @Route('/tech-dashboard/acquisition-system/{id}/install', name: 'app_tech_install_acquisition_system')
+     *
+     * Installe un système d'acquisition en mettant à jour l'attribut isInstalled à 1.
+     *
+     * @param int $id L'identifiant du système d'acquisition à installer
+     * @param ManagerRegistry $doctrine Le registre de gestionnaire d'entités
+     * @return RedirectResponse
+     */
+    #[Route('/tech-dashboard/acquisition-system/{id}/install', name: 'app_tech_install_acquisition_system')]
+    #[IsGranted("ROLE_TECHNICIAN")]
+    public function installAcquisitionSystem(int $id, ManagerRegistry $doctrine): RedirectResponse
     {
-        return $this->render('tech/index.html.twig', [
-            'controller_name' => 'TechController',
-        ]);
+        $entityManager = $doctrine->getManager();
+
+        $acquisitionSystemRepository = $entityManager->getRepository('App\Entity\AcquisitionSystem');
+
+        // Récupérer le système d'acquisition par son ID
+        $acquisitionSystem = $acquisitionSystemRepository->find($id);
+
+        if (!$acquisitionSystem) {
+            // Gérer le cas où le système d'acquisition n'est pas trouvé
+            throw $this->createNotFoundException('Le système d\'acquisition n\'existe pas');
+        }
+
+        // Mettre à jour l'attribut isInstalled à 1
+        $acquisitionSystem->setIsInstalled(true);
+
+        // Enregistrer les modifications
+        $entityManager->persist($acquisitionSystem);
+        $entityManager->flush();
+
+        // Ajouter un message flash
+        $this->addFlash('success', 'Le système d\'acquisition a été installé avec succès.');
+
+        // Rediriger vers une autre page après l'installation
+        return $this->redirectToRoute('app_tech_dashboard');
     }
 }

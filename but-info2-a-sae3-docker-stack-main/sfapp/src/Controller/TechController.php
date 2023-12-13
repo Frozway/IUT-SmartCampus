3b0794cffca8e564138444ab546a220f35a83a79
Achille Dupont <achille.dupont@etudiant.univ-lr.fr>
2023-12-13 21:47:03 +0100

    feat (tech-dashboard) : tech can install and uninstall when needed + color codes

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/TechController.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Controller/TechController.php
@@ -49,4 +49,43 @@ class TechController extends AbstractController
         // Rediriger vers une autre page après l'installation
         return $this->redirectToRoute('app_tech_dashboard');
     }
+
+    /**
+     * @Route('/tech-dashboard/acquisition-system/{id}/install', name: 'app_tech_uninstall_acquisition_system')
+     *
+     * Désinstalle un système d'acquisition en mettant à jour l'attribut isInstalled à 0.
+     *
+     * @param int $id L'identifiant du système d'acquisition à installer
+     * @param ManagerRegistry $doctrine Le registre de gestionnaire d'entités
+     * @return RedirectResponse
+     */
+    #[Route('/tech-dashboard/acquisition-system/{id}/uninstall', name: 'app_tech_uninstall_acquisition_system')]
+    #[IsGranted("ROLE_TECHNICIAN")]
+    public function uninstallAcquisitionSystem(int $id, ManagerRegistry $doctrine): RedirectResponse
+    {
+        $entityManager = $doctrine->getManager();
+
+        $acquisitionSystemRepository = $entityManager->getRepository('App\Entity\AcquisitionSystem');
+
+        // Récupérer le système d'acquisition par son ID
+        $acquisitionSystem = $acquisitionSystemRepository->find($id);
+
+        if (!$acquisitionSystem) {
+            // Gérer le cas où le système d'acquisition n'est pas trouvé
+            throw $this->createNotFoundException('Le système d\'acquisition n\'existe pas');
+        }
+
+        // Mettre à jour l'attribut isInstalled à 0
+        $acquisitionSystem->setIsInstalled(false);
+
+        // Enregistrer les modifications
+        $entityManager->persist($acquisitionSystem);
+        $entityManager->flush();
+
+        // Ajouter un message flash
+        $this->addFlash('success', 'Le système d\'acquisition a été désinstallé avec succès.');
+
+        // Rediriger vers une autre page après l'installation
+        return $this->redirectToRoute('app_tech_dashboard');
+    }
 }
--- but-info2-a-sae3-docker-stack-main/sfapp/src/Entity/AcquisitionSystem.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Entity/AcquisitionSystem.php
@@ -33,6 +33,10 @@ class AcquisitionSystem
     #[ORM\Column]
     private ?bool $isInstalled = null;
 
+    // 0 = OK, 1 = doit être installé, 2 = doit être désinstallé
+    #[ORM\Column]
+    private ?int $state = null;
+
     /**
      * AcquisitionSystem constructor.
      * Initialise la propriété isInstalled à false.
@@ -41,6 +45,9 @@ class AcquisitionSystem
     {
         // Initialiser isInstalled avec la valeur 0 (false) lors de la création de l'instance
         $this->isInstalled = false;
+
+        // Initialiser state avec la valeur 0 lors de la création de l'instance
+        $this->state = 0;
     }
 
     /**
@@ -143,6 +150,18 @@ class AcquisitionSystem
     public function setIsInstalled(bool $isInstalled): static
     {
         $this->isInstalled = $isInstalled;
+
+        return $this;
+    }
+
+    public function getState(): ?int
+    {
+        return $this->state;
+    }
+
+    public function setState(int $state): static
+    {
+        $this->state = $state;
 
         return $this;
     }

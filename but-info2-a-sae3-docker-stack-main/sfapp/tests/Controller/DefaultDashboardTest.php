a12657ab8332ff8fe2935ef9b4fe6cf3a62f7e01
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-01-16 16:03:59 +0100

    test(DefaultDashboard & TechController): adding test for TechController and Refactor DefaultDashboard's test for a better errors management

--- but-info2-a-sae3-docker-stack-main/sfapp/tests/Controller/DefaultDashboardTest.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/tests/Controller/DefaultDashboardTest.php
@@ -8,32 +8,45 @@ use Symfony\Component\HttpClient\Response\MockResponse;
 
 class DefaultDashboardTest extends WebTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->client = static::createClient();
-        $this->numberOfRooms = $this->client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->countRooms();
-        $this->firstRoomId = $this->client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->getFirstRoomId();
-    }
 
     public function testAccessPage(): void
     {
-        $this->crawler = $this->client->request('GET', '/');
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+
+        // ************************* TEST **************************** \\
 
         $this->assertResponseIsSuccessful();
     }
 
     public function testBlockTitle(): void
     {
-        $this->crawler = $this->client->request('GET', '/');
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+
+        // ************************* TEST **************************** \\
 
         $this->assertSelectorTextContains('head title', 'Acceuil | SmartCampus');
     }
 
     public function testUserNavbar(): void
     {
-        $this->crawler = $this->client->request('GET', '/');
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+
+        // ************************* TEST **************************** \\
 
         // Seule la navbar d'un utilisateur non connecté contient l'élément "Login"
         $this->assertSelectorExists('div.navbar a img[alt="Login"]');
@@ -41,7 +54,17 @@ class DefaultDashboardTest extends WebTestCase
 
     public function testUserRoomList(): void
     {
-        $this->crawler = $this->client->request('GET', '/');
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $crawler= $client->request('GET', '/');
+
+        // Récupérer le nombre de salles dans la base de données
+        $numberOfRooms = $client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->countRooms();
+
+
+        // ************************* TEST **************************** \\
 
         // Seule la liste d'un utilisateur non connecté n'a pas le bouton "ajouter" contenu dans une div avec id="AddRoomAdmin"
         $this->assertSelectorNotExists('div#AddRoomAdmin button');
@@ -50,24 +73,34 @@ class DefaultDashboardTest extends WebTestCase
         $this->assertSelectorTextContains('div', 'Salles');
 
         // Cette liste contient bien autant de li que de salle dans la base de données
-        $this->assertEquals($this->numberOfRooms, $this->crawler->filter('li')->count());
+        $this->assertEquals($numberOfRooms, $crawler->filter('li')->count());
     }
 
     public function testUserRoomDetailsLink(): void
     {
-        $this->crawler = $this->client->request('GET', '/');
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $crawler = $client->request('GET', '/');
+
+        // Récupérer l'id de la première salle
+        $firstRoomId = $client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->getFirstRoomId();
+
+
+        // ************************* TEST **************************** \\
 
         // Récupérer le lien de la première salle
-        $link = $this->crawler->filter('ul#RoomList a#Room')->first();
+        $link = $crawler->filter('ul#RoomList a#Room')->first();
 
         // Vérifier que le lien existe
         $this->assertNotEmpty($link);
 
         // Vérifier que le lien est bien celui qui redirige vers la page de détails de la première salle
-        $this->assertEquals('/room/' . $this->firstRoomId, $link->attr('href'));
+        $this->assertEquals('/room/' . $firstRoomId, $link->attr('href'));
 
         // Cliquer sur la salle pour accéder à la page de détails de la salle
-        $this->client->click($link->link());
+        $crawler= $client->request('GET', '/room/' . $firstRoomId);
 
         // Vérifier que les détails de la salle sont bien affichés
         $this->assertSelectorExists('div#UserRoomDetails');
@@ -75,26 +108,44 @@ class DefaultDashboardTest extends WebTestCase
 
     public function testEmptyDataHandling(): void
     {
-        // Créez un mock pour HttpClientInterface pour simuler un appel à l'API vide
-        $mockHttpClient = new MockHttpClient([new MockResponse('{}')]);
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
 
-        // Utilisez le mock à la place du service HttpClientInterface réel
-        $container = $this->client->getContainer();
-        $container->set('http_client', $mockHttpClient);
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+        // Récupérer l'id de la première salle
+        $firstRoomId = $client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->getFirstRoomId();
 
         // Effectuez la requête GET vers la page
-        $this->client->request('GET', '/room/' . $this->firstRoomId);
+        $client->request('GET', '/room/' . $firstRoomId);
 
-        // Assertions - Vérifiez que la page affiche un message informatif lorsque data est vide
+
+        // ************************* TEST **************************** \\
+
+        // On sait que l'appel à l'API n'étant pas possible depuis ces tests
+        // On vérifie alors que la page informe bien l'utilisateur qu'aucune donnée n'est disponible
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('.alert-info', 'Aucune donnée à afficher');
     }
 
     public function testChartDataCreation(): void
     {
-        $this->client->request('GET', '/room/' . $this->firstRoomId);
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
 
-        // Vérifiez le comportement du composant roomDataCharts lors de la création des graphiques
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+        // Récupérer l'id de la première salle
+        $firstRoomId = $client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->getFirstRoomId();
+
+        $client->request('GET', '/room/' . $firstRoomId);
+
+
+        // ************************* TEST **************************** \\
+
+        // On vérifie que les graphiques sont bien créés dans la page
         $this->assertSelectorExists('#temperatureChart');
         $this->assertSelectorExists('#co2Chart');
         $this->assertSelectorExists('#humidityChart');
@@ -102,19 +153,21 @@ class DefaultDashboardTest extends WebTestCase
 
     public function testDataLimit864(): void
     {
-        $this->client->request('GET', '/room/' . $this->firstRoomId, ['dataLimit' => 864]);
+        // ************************ SETUP **************************** \\
+        $client = static::createClient();
+
+        // Accéder à la page d'accueil hors connexion
+        $client->request('GET', '/');
+
+        // Récupérer l'id de la première salle
+        $firstRoomId = $client->getContainer()->get('doctrine')->getRepository('App\Entity\Room')->getFirstRoomId();
+
+        $client->request('GET', '/room/' . $firstRoomId, ['dataLimit' => 864]);
+
+
+        // ************************* TEST **************************** \\
 
         // Assertions spécifiques pour dataLimit = 864
         $this->assertResponseIsSuccessful();
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        // Libérer les ressources si nécessaire
-        $this->client = null;
-        $this->crawler = null;
-        $this->numberOfRooms = null;
     }
 }

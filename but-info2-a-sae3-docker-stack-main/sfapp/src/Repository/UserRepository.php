3c684669785d0d2f586573aa12204fcd2097cba6
Thibaut Lefrancois <thibaut.lefrancois@etudiant.univ-lr.fr>
2024-01-17 22:21:42 +0100

    test(TechController):  adding test for the Tech Controller

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Repository/UserRepository.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Repository/UserRepository.php
@@ -21,6 +21,17 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    //Fonction qui retourne l'utilisateur tech username
+    public function findTechUser()
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.username = :val')
+            ->setParameter('val', 'tech')
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */

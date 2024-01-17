f74e5eb4aef135932a067949c932a93f2d8f0d7e
Achille Dupont <achille.dupont@etudiant.univ-lr.fr>
2024-01-18 00:21:29 +0100

    fix (error types) : fix showing erros when duplicate names are entered

--- but-info2-a-sae3-docker-stack-main/sfapp/src/Form/DepartmentType.php
+++ but-info2-a-sae3-docker-stack-main/sfapp/src/Form/DepartmentType.php
@@ -4,15 +4,22 @@ namespace App\Form;
 
 use App\Entity\Department;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
+/**
+ * Formulaire de création pour la classe Department.
+ *
+ * @package App\Form
+ */
 class DepartmentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class)
         ;
     }
 
